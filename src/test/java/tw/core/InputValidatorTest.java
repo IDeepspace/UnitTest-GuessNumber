1beac21dd90ecc2ef2b32ec8b11bc9d19c9317fc
IDeepspace <cxin1427@qq.com>
2018-04-09 16:11:46 +0800

    add InputValidatorTest

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,55 @@
 package tw.core;
 
-/**
- * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
- */
+import org.junit.Test;
+import tw.validator.InputValidator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class InputValidatorTest {
 
+  @Test
+  public void should_return_true_when_input_right_num() throws Exception {
+
+    InputValidator inputValidator = new InputValidator();
+
+    String num = "1 2 3 4";
+
+    Boolean result = inputValidator.validate(num);
+
+    assertThat(result, is(true));
+  }
+
+  @Test
+  public void should_return_false_when_input_fault_num_for_two_repetition_digit() throws Exception {
+    InputValidator inputValidator = new InputValidator();
+
+    String num = "1 1 2 3";
+
+    Boolean result = inputValidator.validate(num);
+
+    assertThat(result, is(false));
+  }
+
+  @Test
+  public void should_return_false_when_input_fault_num_for_less_four_digit() throws Exception {
+    InputValidator inputValidator = new InputValidator();
+
+    String num = "1 2 3";
+
+    Boolean result = inputValidator.validate(num);
+
+    assertThat(result, is(false));
+  }
+
+  @Test
+  public void should_return_false_when_input_fault_num_for_a_digit_more_than_9() throws Exception {
+    InputValidator inputValidator = new InputValidator();
+
+    String num = "1 2 3 10";
+
+    Boolean result = inputValidator.validate(num);
+
+    assertThat(result, is(false));
+  }
 }
