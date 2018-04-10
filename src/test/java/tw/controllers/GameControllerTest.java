bdca37bf51cbab717b25ad347ee236fcad99b321
IDeepspace <cxin1427@qq.com>
2018-04-10 15:41:00 +0800

    add test

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -1,8 +1,37 @@
 package tw.controllers;
 
-/**
- * 在GameControllerTest文件中完成GameController中对应的单元测试
- */
-public class GameControllerTest {
+import tw.core.Answer;
+import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.generator.AnswerGenerator;
+import tw.core.generator.RandomIntGenerator;
+import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class GameControllerTest {
+  @Test(expected = OutOfRangeAnswerException.class)
+  public void should_throw_OutOfRangeAnswerException_which_is_not_between_0_and_9() throws OutOfRangeAnswerException {
+    RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
+    when(randomIntGenerator.generateNums(anyInt(), anyInt())).thenReturn("1 2 3 15");
+    AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+
+    answerGenerator.generate();
+
+  }
+
+  @Test
+  public void should_get_radam_number() throws Exception {
+    RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
+    when(randomIntGenerator.generateNums(anyInt(), anyInt())).thenReturn("1 2 3 4");
+    AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+
+    Answer answer = answerGenerator.generate();
+    assertNotNull(answer);
+    assertThat(answer.getIndexOfNum("2"), is(1));
+  }
 }
--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,8 +1,53 @@
 package tw.core;
 
-/**
- * 在AnswerTest文件中完成Answer中对应的单元测试
- */
-public class AnswerTest {
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.model.Record;
+
+public class AnswerTest {
+  private Answer answer;
+
+  @Before
+  public void init() throws Exception {
+    answer = Answer.createAnswer("1 2 3 4");
+  }
+
+  @Test
+  public void should_return_0A0B_when_no_number_is_correct() {
+    String inputAnswerStr = "5 6 7 8";
+    String expectValue = "0A0B";
+    validateInputAnswerStr(inputAnswerStr,expectValue);
+  }
+
+  @Test
+  public void should_return_4A0B_when_all_numbers_are_correct() {
+    String inputAnswerStr = "1 2 3 4";
+    String expectValue = "4A0B";
+    validateInputAnswerStr(inputAnswerStr,expectValue);
+  }
+
+  @Test
+  public void should_return_0A4B_when_all_numbers_are_include_() {
+    String inputAnswerStr = "5 6 7 8";
+    String expectValue = "0A0B";
+    validateInputAnswerStr(inputAnswerStr,expectValue);
+  }
+
+  @Test
+  public void should_return_1A1B_when_1_number_correct_and_1_number_include() {
+    String inputAnswerStr = "1 3 5 6";
+    String expectValue = "1A1B";
+    validateInputAnswerStr(inputAnswerStr,expectValue);
+  }
+
+  private void validateInputAnswerStr(String inputAnswerStr,String expectValue) {
+    Answer inputAnswer = Answer.createAnswer(inputAnswerStr);
+    Record result = answer.check(inputAnswer);
+    int[] existRecord = result.getValue();
+    String s = String.format("%1$sA%2$sB", existRecord[0], existRecord[1]);
+    assertThat(s, is(expectValue));
+  }
 }
